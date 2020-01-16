535f53a0357cb1fbb81d82a85c80a0cc469bc16c
SasmiGurung <np03a180017@heraldcollege.edu.np>
2020-01-16 23:12:34 +0545

    added models

--- questionPlatform/models.py
+++ questionPlatform/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+#creating question class to post the question and store the posted questions in the database
+class Question(models.Model):
+    question_text = models.CharField(max_length=300)
+    post_date = models.DateTimeField('Post Date')
+
+    def __str__(self):
+        return self.question_text
+
+class Answer(models.Model):
+        question = models.ForeignKey(Question, on_delete=models.CASCADE)
+        answer_text = models.CharField(max_length=1000)
+        is_favorite = models.BooleanField(default=False)
+
+        def __str__(self):
+            return self.question
+
+
