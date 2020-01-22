b58b9f0358053df6e676a7c34b560bcf7053b9b6
Sasmi Gurung <sasmigrg93@gmail.com>
2020-01-22 07:52:31 +0545

    question form created

--- questionPlatform/forms.py
+++ questionPlatform/forms.py
@@ -1,14 +1,10 @@
 from django import forms
+from questionPlatform.models import Question
+# from .models import Answer
 
-from .models import Question
-from .models import Answer
 
-class OurForm(forms.ModelForm):
-    #form betra meta data
+class questionForm(forms.ModelForm):
+    # form betra meta data
     class Meta:
         model = Question
-        fields = ('Question_text', 'post_date')
-
-    class Meta:
-        model = Answer
-        fields = ('question', 'answer_text', 'is_favorite')
+        fields = ('question_text', 'post_date')
