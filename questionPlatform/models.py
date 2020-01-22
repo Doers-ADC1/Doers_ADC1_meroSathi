bbeeb2f509e59b1941d193a05a6c894049734b7a
Sasmi Gurung <sasmigrg93@gmail.com>
2020-01-22 07:52:47 +0545

    model updated

--- questionPlatform/models.py
+++ questionPlatform/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-#creating question class to post the question and store the posted questions in the database
+
+# creating question class to post the question and store the posted questions in the database
 class Question(models.Model):
     question_text = models.CharField(max_length=300)
     post_date = models.DateTimeField('Post Date')
@@ -8,12 +9,12 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+
 class Answer(models.Model):
-        question = models.ForeignKey(Question, on_delete=models.CASCADE)
-        answer_text = models.CharField(max_length=1000)
-        is_favorite = models.BooleanField(default=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=1000)
+    # is_favorite = models.BooleanField(default=False)
 
-        def __str__(self):
-            return self.question
-
+    def __str__(self):
+        return self.answer_text
 
