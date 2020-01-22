b9b39ade52036bdd6ed57c05ee00ca27df747905
Sasmi Gurung <sasmigrg93@gmail.com>
2020-01-22 07:53:54 +0545

    compile file updated

---
+++ questionPlatform/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from questionPlatform import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('add/', views.add_questions, name="add_question"),
+    path('question/answers/<id>/', views.answers, name="questions_answers"),
+]
---
+++ sathi/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/rosan/Desktop/PlayGround/Doers_ADC1_meroSathi/sathi/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
