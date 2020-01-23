809c0a0d60369d6b45ef5bfc3ac4b1925bde39d1
Sasmi Gurung <sasmigrg93@gmail.com>
2020-01-23 23:16:22 +0545

    delete endpoint created

--- questionPlatform/urls.py
+++ questionPlatform/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('add/', views.add_questions, name="add_question"),
     path('question/answers/<id>/', views.answers, name="questions_answers"),
+    path('question/post_ans/<id>/', views.post_answer, name="post_answers"),
+    path('quesion/delete/<id>', views.delete_question, name="delete_question"),
+
 ]
