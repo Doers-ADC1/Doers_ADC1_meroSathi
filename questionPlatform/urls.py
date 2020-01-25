142bf082d8da9169208f0d5430c31159c8470469
Sasmita Gurung <sasmigrg93@gmail.com>
2020-01-25 10:55:55 +0545

    edit endpoint created

--- questionPlatform/urls.py
+++ questionPlatform/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('add/', views.add_questions, name="add_question"),
     path('question/answers/<id>/', views.answers, name="questions_answers"),
     path('question/post_ans/<id>/', views.post_answer, name="post_answers"),
+    path('question/edit_ques/<id>', views.edit_question, name = "edit_question"),
     path('quesion/delete/<id>', views.delete_question, name="delete_question"),
 ]
