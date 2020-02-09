871d80bd0261adca7766bb268122584cbc9a254c
Sasmita Gurung <sasmigrg93@gmail.com>
2020-02-10 04:06:22 +0545

    templates created

--- userprofile/Doers_ADC1_meroSathi/README.md
+++
@@ -1,9 +0,0 @@
-# Doers_ADC1_meroSathi
-
-## Project Name: meroSathi
-
-Team Members
-+ Sasmita Gurung
-+ Aishwarya Gurung
-+ Tsering Sherpa
-+ Suresh Karki
--- userprofile/Doers_ADC1_meroSathi/userprofile/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from userprofile.models import Profile, User
-# Register your models here.
-
-admin.site.register(Profile)
