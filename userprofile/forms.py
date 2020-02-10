2598843e8eb1b3393c394c3f9e8232b2bd5fd017
Sasmita Gurung <sasmigrg93@gmail.com>
2020-02-10 22:54:15 +0545

    user profile update form created

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -9,6 +9,14 @@ class RegistrationForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+        exclude = ['user']
+
+
 # class RegistrationForm(UserCreationForm):
 #     email = forms.EmailField(required=True)
 #
